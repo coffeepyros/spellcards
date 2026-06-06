14ed664e042fbf9e564ec7aa32cd328542d37526
Christian Heinrich <coffeepyros@gmail.com>
2026-06-06 13:30:09 +0200

    dancing with cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+// DATA
 import { data as srdData } from "./data.js";
 import { customData } from "./customData.js";
-
 const data = srdData.concat(customData);
 
 // FILTER
 const classFilter = "Wizard";
 const minLevel = 0;
 const maxLevel = 1;
+
+// console.log(data.length);
 
 const spells = data.filter(
   (spell) =>
@@ -16,8 +18,22 @@ const spells = data.filter(
     spell.level <= maxLevel,
 );
 
-console.log(spells.length);
+// console.log(spells.length);
 
 // RENDER
 
 const layout = document.getElementById("layout");
+const displaySpells = spells.slice(0, 9);
+
+displaySpells.forEach((spell) => {
+  const spellElement = document.createElement("section");
+  spellElement.classList.add("card");
+
+  const nameElement = document.createElement("h2");
+  nameElement.classList.add("name");
+  nameElement.innerText = spell.spell_name;
+
+  spellElement.append(nameElement);
+
+  layout.append(spellElement);
+});
